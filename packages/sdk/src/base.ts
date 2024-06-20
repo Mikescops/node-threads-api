6388b115dd1915141e3229e22265379ada36510d
Corentin Mors <corentin@dashlane.com>
2024-06-20 14:42:19 +0200

    Divide sdk in namespaces (#2)

---
+++ packages/sdk/src/base.ts
@@ -0,0 +1,62 @@
+import got from 'got';
+import { AuthParams, ErrorResponse, TokenParams, TokenResponse } from './types';
+
+export class BaseResource {
+    public graphApiVersion = 'v1.0';
+    public graphApiBaseUrl = 'https://graph.threads.net/' + (this.graphApiVersion ? this.graphApiVersion + '/' : '');
+    public authorizationBaseUrl = 'https://threads.net';
+
+    public buildGraphApiUrl = (
+        path: string,
+        searchParams: Record<string, string>,
+        accessToken?: string | null,
+        base_url?: string | null
+    ) => {
+        const url = new URL(path, base_url ?? this.graphApiBaseUrl);
+
+        url.search = new URLSearchParams(searchParams).toString();
+        if (accessToken) {
+            url.searchParams.append('access_token', accessToken);
+        }
+
+        return url.toString();
+    };
+
+    public getAuthorizationUrl = (params: AuthParams): string => {
+        const url = this.buildGraphApiUrl(
+            'oauth/authorize',
+            {
+                ['scope']: params.scopes.join(','),
+                ['client_id']: params.clientId,
+                ['redirect_uri']: params.redirectUri,
+                ['response_type']: 'code',
+            },
+            null,
+            this.authorizationBaseUrl
+        );
+
+        return url;
+    };
+
+    public exchangeCodeForToken = async (params: TokenParams): Promise<TokenResponse> => {
+        const uri = this.buildGraphApiUrl('oauth/access_token', {}, null, this.graphApiBaseUrl);
+
+        const response = await got.post(uri, {
+            form: {
+                client_id: params.clientId,
+                client_secret: params.clientSecret,
+                code: params.code,
+                grant_type: 'authorization_code',
+                redirect_uri: params.redirectUri,
+            },
+            responseType: 'json',
+            throwHttpErrors: false,
+        });
+
+        if (response.statusCode === 200) {
+            return response.body as TokenResponse;
+        } else {
+            throw new Error((response.body as ErrorResponse).error.message);
+        }
+    };
+}
